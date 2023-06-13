552ef3b0a4ceef4f2bbd4a75b92ef4ff00ac8243
fsantama <fransantamaria95@gmail.com>
2023-06-13 12:03:02 +0200

    ft_check_args.c modified

--- src/ft_check_args.c
+++ src/ft_check_args.c
@@ -6,7 +6,7 @@
 /*   By: fsantama <fsantama@student.42malaga.com>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/13 11:24:35 by fsantama          #+#    #+#             */
-/*   Updated: 2023/06/13 11:42:21 by fsantama         ###   ########.fr       */
+/*   Updated: 2023/06/13 12:02:26 by fsantama         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,15 +20,9 @@ int	ft_check_args(char **argv);
 	while (argv[i])
 	{
 		if (ft_check_num(argv[i]) == 1)
-		{
-			ft_putstr_fd("Número no válido\n", 2);
 			return (1);
-		}
 		if (ft_check_limits(argv[i]) == 1)
-		{
-			ft_putstr_fd("Límites excedidos\n", 2);
 			return (1)
-		}
 		i++;
 	}
 	return (0);
